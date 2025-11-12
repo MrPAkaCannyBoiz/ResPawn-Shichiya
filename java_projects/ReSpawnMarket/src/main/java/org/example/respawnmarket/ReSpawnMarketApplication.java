9139aba46ed9c2d8b9f82fc6d392fe19e9194ecd
MrPAkaCannyBoiz <pongsatorn5762@gmail.com>
2025-11-12 12:42:49 +0100

    test test

--- java_projects/ReSpawnMarket/src/main/java/org/example/respawnmarket/ReSpawnMarketApplication.java
+++ java_projects/ReSpawnMarket/src/main/java/org/example/respawnmarket/ReSpawnMarketApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReSpawnMarketApplication
 {
+    // load .env file
+
+
 
     public static void main(String[] args)
     {
