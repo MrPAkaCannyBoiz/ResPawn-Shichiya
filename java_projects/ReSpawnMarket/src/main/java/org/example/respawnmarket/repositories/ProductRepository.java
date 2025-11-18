ddb961a5a9eaac01f57d4ffe0e576477825b9fe1
KarinaRub <107041774+KarinaRub@users.noreply.github.com>
2025-11-18 21:10:02 +0100

    Created ProductInspectionServiceImpl

--- java_projects/ReSpawnMarket/src/main/java/org/example/respawnmarket/repositories/ProductRepository.java
+++ java_projects/ReSpawnMarket/src/main/java/org/example/respawnmarket/repositories/ProductRepository.java
@@ -1,8 +1,12 @@
 package org.example.respawnmarket.repositories;
 
+import java.util.List;
+
 import org.example.respawnmarket.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer>
 {
+    List<ProductEntity> findByApprovalStatus(String approvalStatus);
 }
+
