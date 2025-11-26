58a17eb1abfc12c9ead749a9e0f61fa30395a4f6
CrisTina119 <cristinalincu19@gmail.com>
2025-11-26 15:56:47 +0100

    login java done

--- java_projects/ReSpawnMarket/src/main/java/org/example/respawnmarket/repositories/ResellerRepository.java
+++ java_projects/ReSpawnMarket/src/main/java/org/example/respawnmarket/repositories/ResellerRepository.java
@@ -3,6 +3,9 @@ package org.example.respawnmarket.repositories;
 import org.example.respawnmarket.entities.ResellerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ResellerRepository extends JpaRepository<ResellerEntity, Integer>
 {
+    Optional<ResellerEntity> findByUsername(String username);
 }
